dd6100bd3023fbfa93d1e08660ebbd8f5300bac1
VladTca <votsats@gmail.com>
2024-03-23 09:04:01 +0100

    "Refactor Profile component and add ProfileInfo subcomponent, update dependencies in package.json, and add styling to MyPosts component"

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -16,18 +16,49 @@ const Message = (props: any) => {
 };
 
 export const Dialogs = () => {
+  let dialogsData = [
+    {
+      id: 1,
+      name: "Dimich",
+    },
+    {
+      id: 2,
+      name: "Andrei",
+    },
+    {
+      id: 3,
+      name: "Sveta",
+    },
+    {
+      id: 4,
+      name: "Viktor",
+    },
+    {
+      id: 5,
+      name: "Valera",
+    },
+    {
+      id: 6,
+      name: "Sascha",
+    },
+    {
+      id: 7,
+      name: "Vladimir",
+    },
+  ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name="Dimich" id="1" />
-        <DialogItem name="Andrei" id="2" />
-        <DialogItem name="Sveta" id="3" />
-        <DialogItem name="Viktor" id="4" />
-        <DialogItem name="Valera" id="5" />
-        <DialogItem name="Sasch" id="6" />
-        <DialogItem name="Vladimir" id="7" />
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+        <DialogItem name={dialogsData[6].name} id={dialogsData[6].id} />
       </div>
-      <div className="messages">
+      <div className={s.messages}>
         <Message message="Hi" />
         <Message message="How are you?" />
         <Message message="Yo" />
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,7 +5,7 @@ export const ProfileInfo = () => {
   return (
     <div>
       <div>
-        <img src="cartoon.jpg" alt="cartoon" />
+        <img src="/cartoon.jpg" alt="cartoon" />
       </div>
       <div className={s.descriptionBlock}>ava + description</div>
     </div>
