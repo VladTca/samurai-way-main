6e2199e843398a2061f2ec48667213c9732d1ab4
VladTca <votsats@gmail.com>
2024-03-23 05:58:11 +0100

    Updated Dialogs component to display a list of dialog items and messages with proper styling in Dialogs.module.css. Added NavLink for dialog items and structured messages within the component.

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,6 +1,38 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 export const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>
+          <NavLink to="dialogs/1">Dimich</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="dialogs/2">Andrei</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="dialogs/3">Sveta</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="dialogs/4">Viktor</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="dialogs/5">Valera</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="dialogs/6">Sveta2</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="dialogs/7">Sveta3</NavLink>
+        </div>
+      </div>
+      <div className="messages">
+        <div className="message">Hi</div>
+        <div className="message">How are you?</div>
+        <div className="message">Yo</div>
+      </div>
+    </div>
+  );
 };
