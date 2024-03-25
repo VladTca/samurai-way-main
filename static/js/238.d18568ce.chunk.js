dc35bbe156bffd3d151457b724b51497c07d36b0
VladTca <votsats@gmail.com>
2024-03-25 21:57:33 +0100

    Updates

---
+++ static/js/238.d18568ce.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksamurai_way=self.webpackChunksamurai_way||[]).push([[238],{2238:function(n,e,r){r.r(e),r.d(e,{default:function(){return d}});var a=r(4306),s=r(2791),t=r(3502),u=r(184),i=function(){return(0,u.jsxs)(a.YI,{children:[(0,u.jsx)(a.WD,{name:"App",children:(0,u.jsx)(a.wA,{name:"Loader",children:(0,u.jsx)(a.$e,{children:(0,u.jsx)(c,{})})})}),(0,u.jsx)(t.Z,{})]})};function c(){return(0,u.jsx)(s.Fragment,{children:"Loading..."})}var d=function(){return(0,u.jsx)(a.Vt,{palette:(0,u.jsx)(i,{})})}}}]);
+//# sourceMappingURL=238.d18568ce.chunk.js.map
