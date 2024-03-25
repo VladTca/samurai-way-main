d310b9eb77e5c6f2532083a1867d964694514e2d
VladTca <votsats@gmail.com>
2024-03-25 23:04:35 +0100

    You exceeded your current quota, please check your plan and billing details. For more information on this error, read the docs: https://platform.openai.com/docs/guides/error-codes/api-errors.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from "./Header.module.css";
+import volk from "./volk.jpg";
 
 export const Header = () => {
   return (
     <header className={s.header}>
-      <img src="volk.jpg" alt="logo" />
+      <img src={volk} alt="logo" />
     </header>
   );
 };
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
+import cartoon from "./cartoon.jpg";
 
 export const ProfileInfo = () => {
   return (
     <div>
       <div>
-        <img src="/cartoon.jpg" alt="cartoon" />
+        <img src={cartoon} alt="cartoon" />
       </div>
       <div className={s.descriptionBlock}>ava + description</div>
     </div>
